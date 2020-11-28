6ef6e3f88f20c55ca1fa597eb0e404f0d23111ad
John Haigh <haighis@gmail.com>
2020-11-27 19:16:50 -0500

    Add Cache Server GenServer with Unit Tests
    - Implement Cache GenServer that consumes Functional Core Cache.Core
    - Refactor Functional Core to not have handle method naming since this will be a library and won't be a GenServer
    - Update Functional Core Unit Tests for new method names

--- test/core_test.exs
+++ test/core_test.exs
@@ -14,23 +14,23 @@ defmodule CoreTest do
   test "get_with_incorrect_key_should_return_error_atom" do
     init(:my)
     # Get a key that does not exist should return an error atom
-    assert handle_get(:my,"1") == :error
+    assert get(:my,"1") == :error
     :ets.delete(:my)
     :ets.delete(:my_ttl)
   end
 
   test "get_should_return_correct_value" do
     init(:my)
-    handle_put(:my,14,"test1",1)
-    assert handle_get(:my,14) == "test1"    
+    put(:my,14,"test1",1)
+    assert get(:my,14) == "test1"    
     :ets.delete(:my)
     :ets.delete(:my_ttl)
   end
 
   test "put_should_store_correct_value" do
     init(:my)
-    handle_put(:my,1,"test1",1)
-    assert handle_get(:my,1) == "test1"    
+    put(:my,1,"test1",1)
+    assert get(:my,1) == "test1"    
     :ets.delete(:my)
     :ets.delete(:my_ttl)
   end
@@ -38,9 +38,9 @@ defmodule CoreTest do
   test "cache_capacity_should_be_correct_size" do
     init(:my)
     # add three items to the cache to validate cache capacity assertion
-    handle_put(:my,1,"test1",2)
-    handle_put(:my,2,"test1",2)
-    handle_put(:my,3,"test1",2)
+    put(:my,1,"test1",2)
+    put(:my,2,"test1",2)
+    put(:my,3,"test1",2)
     assert :ets.info(:my, :size) == 2
     assert :ets.info(:my_ttl, :size) == 2    
     :ets.delete(:my)
@@ -49,22 +49,22 @@ defmodule CoreTest do
 
   test "cache_should_return_recently_used" do
     init(:my)
-    handle_put(:my,1,"test1",2)
-    handle_put(:my,2,"test2",2)
-    handle_put(:my,3,"test3",2)
-    handle_get(:my,1)
-    assert handle_get(:my,1) == :error
-    assert handle_get(:my,2) == "test2"
-    assert handle_get(:my,3) == "test3"
+    put(:my,1,"test1",2)
+    put(:my,2,"test2",2)
+    put(:my,3,"test3",2)
+    get(:my,1)
+    assert get(:my,1) == :error
+    assert get(:my,2) == "test2"
+    assert get(:my,3) == "test3"
     :ets.delete(:my)
     :ets.delete(:my_ttl)
   end
 
   test "multiple_data_types_for_key_should_be_supported" do
     init(:my)  
-    handle_put(:my,1,"test1",3)
-    handle_put(:my,"2","test2",3)
-    handle_put(:my,:three,"test3",3)
+    put(:my,1,"test1",3)
+    put(:my,"2","test2",3)
+    put(:my,:three,"test3",3)
     [{key1, _, _}] = :ets.lookup(:my, 1)
     assert key1 == 1 
     [{key2, _, _}] = :ets.lookup(:my, "2")
@@ -75,20 +75,20 @@ defmodule CoreTest do
     :ets.delete(:my_ttl)
   end
 
-  test "handle_put_multiple_data_types_for_values_should_be_supported" do
+  test "put_multiple_data_types_for_values_should_be_supported" do
     cache_name = :my
     cache_size = 10
     init(cache_name)  
-    handle_put(cache_name,1,1,cache_size) #integer
-    handle_put(cache_name,2,"test2",cache_size) # string
-    handle_put(cache_name,3,:three,cache_size) # atom
-    handle_put(cache_name,4,[1, 2, 3],cache_size) # list
-    handle_put(cache_name,5,{1, 2, 3},cache_size) # tuple
-    handle_put(cache_name,6,0x1F,cache_size) # integer
-    handle_put(cache_name,7,1.0,cache_size) # float
-    handle_put(cache_name,8,true,cache_size) # boolean
-    handle_put(cache_name,9,%{"hello" => "world", a: 1, b: 2},cache_size) # map
-    handle_put(cache_name,10,<< 65, 68, 75>>,cache_size) # binary
+    put(cache_name,1,1,cache_size) #integer
+    put(cache_name,2,"test2",cache_size) # string
+    put(cache_name,3,:three,cache_size) # atom
+    put(cache_name,4,[1, 2, 3],cache_size) # list
+    put(cache_name,5,{1, 2, 3},cache_size) # tuple
+    put(cache_name,6,0x1F,cache_size) # integer
+    put(cache_name,7,1.0,cache_size) # float
+    put(cache_name,8,true,cache_size) # boolean
+    put(cache_name,9,%{"hello" => "world", a: 1, b: 2},cache_size) # map
+    put(cache_name,10,<< 65, 68, 75>>,cache_size) # binary
     [{_, _, val1}] = :ets.lookup(cache_name, 1)
     assert val1 == 1 
     [{_, _, val2}] = :ets.lookup(cache_name, 2)
@@ -113,48 +113,48 @@ defmodule CoreTest do
     :ets.delete(:my_ttl)
   end
 
-  test "handle_get_multiple_data_types_for_values_should_be_supported" do
+  test "get_multiple_data_types_for_values_should_be_supported" do
     cache_name = :my
     cache_size = 10
     init(cache_name)  
-    handle_put(cache_name,1,1,cache_size) #integer
-    handle_put(cache_name,2,"test2",cache_size) # string
-    handle_put(cache_name,3,:three,cache_size) # atom
-    handle_put(cache_name,4,[1, 2, 3],cache_size) # list
-    handle_put(cache_name,5,{1, 2, 3},cache_size) # tuple
-    handle_put(cache_name,6,0x1F,cache_size) # integer
-    handle_put(cache_name,7,1.0,cache_size) # float
-    handle_put(cache_name,8,true,cache_size) # boolean
-    handle_put(cache_name,9,%{"hello" => "world", a: 1, b: 2},cache_size) # map
-    handle_put(cache_name,10,<< 65, 68, 75>>,cache_size) # binary
-    val1 = handle_get(cache_name,1)
+    put(cache_name,1,1,cache_size) #integer
+    put(cache_name,2,"test2",cache_size) # string
+    put(cache_name,3,:three,cache_size) # atom
+    put(cache_name,4,[1, 2, 3],cache_size) # list
+    put(cache_name,5,{1, 2, 3},cache_size) # tuple
+    put(cache_name,6,0x1F,cache_size) # integer
+    put(cache_name,7,1.0,cache_size) # float
+    put(cache_name,8,true,cache_size) # boolean
+    put(cache_name,9,%{"hello" => "world", a: 1, b: 2},cache_size) # map
+    put(cache_name,10,<< 65, 68, 75>>,cache_size) # binary
+    val1 = get(cache_name,1)
     assert val1 == 1 
-    val2 = handle_get(cache_name, 2)
+    val2 = get(cache_name, 2)
     assert val2 == "test2"
-    val3 = handle_get(cache_name, 3)
+    val3 = get(cache_name, 3)
     assert val3 == :three
-    val4 = handle_get(cache_name, 4)
+    val4 = get(cache_name, 4)
     assert val4 == [1, 2, 3]
-    val5 = handle_get(cache_name, 5)
+    val5 = get(cache_name, 5)
     assert val5 == {1, 2, 3}
-    val6 = handle_get(cache_name, 6)
+    val6 = get(cache_name, 6)
     assert val6 == 0x1F
-    val7 = handle_get(cache_name, 7)
+    val7 = get(cache_name, 7)
     assert val7 == 1.0
-    val8 = handle_get(cache_name, 8)
+    val8 = get(cache_name, 8)
     assert val8 == true
-    val9 = handle_get(cache_name, 9)
+    val9 = get(cache_name, 9)
     assert val9 == %{"hello" => "world", a: 1, b: 2}
-    val10 = handle_get(cache_name, 10)
+    val10 = get(cache_name, 10)
     assert val10 == << 65, 68, 75>>
     :ets.delete(cache_name)
     :ets.delete(:my_ttl)
   end
 
-  test "handle_delete_should_delete_values_correctly" do
+  test "delete_should_delete_values_correctly" do
     init(:my)  
-    handle_put(:my,1,"test1",2)
-    handle_delete(:my,1)
+    put(:my,1,"test1",2)
+    delete(:my,1)
     # Assert key in core cache table and ttl table is empty
     assert :ets.info(:my, :size) == 0
     assert :ets.info(:my_ttl, :size) == 0
